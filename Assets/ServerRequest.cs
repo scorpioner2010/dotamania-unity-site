22b3f3a40e1995eaaa9c883881f1e5507faf6e8c
Taras Samaryn <samarintaras@gmail.com>
2025-03-04 19:36:21 +0800

    added fix

--- Assets/ServerRequest.cs
+++ Assets/ServerRequest.cs
@@ -1,30 +1,50 @@
 using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
+using System.Text;
+using System;
+using TMPro;
+using UnityEngine.UI;
 
 public class ServerRequest : MonoBehaviour
 {
-    private string serverUrl = "https://dotamania.bsite.net/api/hello";
+    //private string serverUrl = "https://dotamania.bsite.net/api"; // URL твого сервера
+    private string serverUrl = "http://localhost:51754/api";
 
+    public Button send;
+    public TMP_InputField sentText;
+    public TMP_Text result;
+    
     void Start()
     {
-        StartCoroutine(SendRequest());
+        send.onClick.AddListener(() =>
+        {
+            StartCoroutine(SendMessageToServer(sentText.text));
+        });
+        
     }
 
-    IEnumerator SendRequest()
+    public IEnumerator SendMessageToServer(string message)
     {
-        using (UnityWebRequest request = UnityWebRequest.Get(serverUrl))
-        {
-            yield return request.SendWebRequest();
+        string jsonData = $"{{\"message\": \"{message}\"}}";
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonData);
 
-            if (request.result == UnityWebRequest.Result.Success)
-            {
-                Debug.Log("Відповідь сервера: " + request.downloadHandler.text);
-            }
-            else
-            {
-                Debug.LogError("Помилка запиту: " + request.error);
-            }
+        UnityWebRequest request = new UnityWebRequest(serverUrl, "POST");
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Відповідь сервера: " + request.downloadHandler.text);
+            result.text = request.downloadHandler.text;
+        }
+        else
+        {
+            Debug.LogError("Помилка запиту: " + request.error);
+            result.text = "error!";
         }
     }
 }
