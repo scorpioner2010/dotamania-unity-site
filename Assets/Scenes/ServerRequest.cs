10ceb9376b3420d9f4e2a2ecf3528531a17a8be9
Taras Samaryn <samarintaras@gmail.com>
2025-03-08 00:01:48 +0800

    fix

--- Assets/Scenes/ServerRequest.cs
+++ Assets/Scenes/ServerRequest.cs
@@ -3,26 +3,51 @@ using UnityEngine.Networking;
 using System.Collections;
 using System.Text;
 using System;
-using NUnit.Framework.Internal;
 using TMPro;
 using UnityEngine.UI;
 
 public class ServerRequest : MonoBehaviour
 {
-    private string serverUrl = "https://dotamania.bsite.net/api"; // URL твого сервера
-    //private string serverUrl = "http://localhost:51754/api";
+    //private string serverUrl = "https://dotamania.bsite.net/api"; // URL твого сервера
+    private string serverUrl = "http://localhost:51754/api";
 
     public Button send;
     public TMP_InputField sentText;
     public TMP_Text result;
+
+    public Image img;
     
-    void Start()
+    private void Start()
     {
         send.onClick.AddListener(() =>
         {
-            StartCoroutine(SendMessageToServer(sentText.text));
+            //StartCoroutine(SendMessageToServer(sentText.text));
+            StartCoroutine(UploadSprite(img.sprite));
         });
+    }
+    
+    private IEnumerator UploadSprite(Sprite sprite)
+    {
+        // Отримання текстури з спрайту
+        Texture2D texture = sprite.texture;
+    
+        // Перетворення текстури в PNG (масив байтів)
+        byte[] imageData = texture.EncodeToPNG();
+    
+        // Створення форми для завантаження
+        WWWForm form = new WWWForm();
+        form.AddBinaryData("file", imageData, "sprite.png", "image/png");
+    
+        // Відправка запиту до серверного endpoint
+        using (UnityWebRequest www = UnityWebRequest.Post(serverUrl+"/upload", form))
+        {
+            yield return www.SendWebRequest();
         
+            if (www.result == UnityWebRequest.Result.Success)
+                Debug.Log("Sprite uploaded successfully!");
+            else
+                Debug.LogError("Upload error: " + www.error);
+        }
     }
 
     public IEnumerator SendMessageToServer(string message)
